fbeb3c87c476655a8627a43e797ad914a41f2750
kakadiya nikul <kakadiyamikul@gmail.com>
2023-04-12 19:47:27 +0530

    325 done and seprat router

--- ng4-complete-guide/src/app/app-routing.module.ts
+++ ng4-complete-guide/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { AuthGuard } from './auth/auth.guard';
+import { AuthComponent } from './auth/auth.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -9,15 +12,8 @@ import { NgModule } from '@angular/core';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    {
-        path: 'recipes', component: RecipesComponent, children: [
-            { path: '', component: RecipeStartComponent },
-            { path: 'new', component: RecipeEditComponent },
-            { path: ':id', component: RecipeDetailComponent },
-            { path: ':id/edit', component: RecipeEditComponent },
-        ]
-    },
     { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }
 ];
 
 @NgModule({
--- ng4-complete-guide/src/app/recipes/recipe.service.ts
+++ ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -8,27 +8,32 @@ import { Ingredient } from '../shared/ingredient.model';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe', 'This is simply a test',
-            'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/shakshouka_74716_16x9.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]
-        ),
-        new Recipe(
-            'A Test Recipe', 'This is simply a test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe', 'This is simply a test',
+    //         'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/shakshouka_74716_16x9.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'A Test Recipe', 'This is simply a test',
+    //         'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ]
+    //     )
+    // ];
 
+    private recipes: Recipe[] = [];
     constructor(private slService: ShoppingListService) { }
 
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
     getRecipes() {
         return this.recipes.slice();
     }
